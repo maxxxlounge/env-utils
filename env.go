ffd4fb852ce81c5bad44dd13e1ec12223d0bffed
massimo.fornasier <massimo.fornasier@pixartprinting.com>
2018-03-16 11:24:42 +0100

    added fmt error print for stout

--- env.go
+++ env.go
@@ -6,6 +6,7 @@ import (
 	"strconv"
 
 	"github.com/pkg/errors"
+	"fmt"
 )
 
 func GetEnv(envKey string) (val string) {
@@ -13,19 +14,46 @@ func GetEnv(envKey string) (val string) {
 	if val == "" {
 		err := errors.Errorf("Env %s does not exists", envKey)
 		log.Fatal(err.Error())
+		fmt.Println(err.Error())
 		os.Exit(1)
 	}
 	return val
 }
 
-func GetEnvInt(envKey string) (val int) {
+func GetEnvInt(envKey string) (val int64) {
+	valString := GetEnv(envKey)
+	val, err := strconv.ParseInt(valString, 10, 64)
+	if err != nil {
+		err = errors.Wrapf(err, "error parsing string %s to int for env %s ", valString, envKey)
+		log.Fatal(err.Error())
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+	return val
+}
+
+func GetEnvInt16(envKey string) (val int16) {
 	valString := GetEnv(envKey)
 	valInt64, err := strconv.ParseInt(valString, 10, 64)
 	if err != nil {
 		err = errors.Wrapf(err, "error parsing string %s to int for env %s ", valString, envKey)
 		log.Fatal(err.Error())
+		fmt.Println(err.Error())
 		os.Exit(1)
 	}
-	val = int(valInt64)
+	val = int16(valInt64)
+	return val
+}
+
+func GetEnvInt8(envKey string) (val int8) {
+	valString := GetEnv(envKey)
+	valInt64, err := strconv.ParseInt(valString, 10, 64)
+	if err != nil {
+		err = errors.Wrapf(err, "error parsing string %s to int for env %s ", valString, envKey)
+		log.Fatal(err.Error())
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+	val = int8(valInt64)
 	return val
 }
